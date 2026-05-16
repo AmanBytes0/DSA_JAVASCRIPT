f0e2c44d6bbab7256158e88607e8042104a8e92d
aman-gound <amangound726482@gmail.com>
2026-05-16 22:27:04 +0530

    practice

--- maxprofit.js
+++ maxprofit.js
@@ -13,3 +13,18 @@ function findmaxprofit(arr) {
   return maxprofit;
 }
 console.log(findmaxprofit(arr));
+
+// let arr = [7, 3, 1, 4, 5, 6, 0];
+// function findmaxprofit(arr) {
+//   let maxprofit = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       let profit = arr[j] - arr[i];
+//       if (profit > maxprofit) {
+//         maxprofit = profit;
+//       }
+//     }
+//   }
+//   return maxprofit;
+// }
+// console.log(findmaxprofit(arr));
