577ed1aaff7c823c388232ab69b6f6ea67acd54c
aman-gound <amangound726482@gmail.com>
2026-05-16 23:22:34 +0530

    practice

---
+++ mergeTwoarray.js
@@ -0,0 +1,22 @@
+let nums1 = [1, 2, 3, 0, 0, 0];
+let nums2 = [2, 5, 6];
+
+function mergearray(nums1, m, nums2, n) {
+  let nums1copy = nums1.slice(0, m);
+  let p1 = 0;
+  let p2 = 0;
+
+  for (let i = 0; i < m + n; i++) {
+    if (p2 >= n || (p1 < m && nums1copy[p1] <= nums2[p2])) {
+      nums1[i] = nums1copy[p1];
+      p1++;
+    } else {
+      nums1[i] = nums2[p2];
+      p2++;
+    }
+  }
+
+  return nums1;
+}
+
+console.log(mergearray(nums1, 3, nums2, 3));
