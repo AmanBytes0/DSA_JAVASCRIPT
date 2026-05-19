fa9dbdfbb6b495d27c8fed6c74d254a556c09e6f
aman-gound <amangound726482@gmail.com>
2026-05-19 09:41:41 +0530

    practice

--- sumofarrayrecursion.js
+++ sumofarrayrecursion.js
@@ -1,6 +1,13 @@
 let arr = [2, 3, 5, 6, 7, 8, 12, 22];
-function findsum(arr) {
-  if (n == 0) return 0;
-  return arr[n] + findsum(arr[n - 1]);
+function findsum(n) {
+  isodd = arr[n] % 2 !== 0;
+  if (n == 0) {
+    if (isodd) {
+      return arr[n];
+    } else return 0;
+  }
+  if (isodd) {
+    return arr[n] + findsum(n - 1);
+  } else return 0 + findsum(n - 1);
 }
-console.log(6);
+console.log(findsum(arr.length - 1));
